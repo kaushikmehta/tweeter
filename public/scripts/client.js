5712acaf7a3e8e18451cab448e6b9e54ea384fd6
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-15 18:53:32 +0000

    fetches and renders tweets with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,38 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- const tweets = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
 const renderTweets = function (tweetData) {
-  const temp = $('#tweets');
-  console.log(temp);
-
+  // console.log("called renderTweets with below");
+  // console.log(tweetData);
   tweetData.forEach(element => {
-    console.log(element);
     const createdTweet = createTweetElement(element);
     $('#tweets').append(createdTweet);
   });
@@ -65,5 +37,28 @@ const renderTweets = function (tweetData) {
 } // function
 
 $(document).ready( () => {
-  renderTweets(tweets);
-})
+  const loadTweets = function () {
+    $.ajax({
+      url:"/tweets",
+      method: "GET"
+    }).then((response) => {
+      renderTweets(response);
+    })
+  };
+
+  loadTweets();
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    const serialized = $(this).serialize();
+
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: serialized
+    }).then(response => {
+      alert("Your tweet is on it's way to tweetHQ. Keep a lookout it'll be displayed here soon!")
+    });
+  }); // form submit
+});
+
